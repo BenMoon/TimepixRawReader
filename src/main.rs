cbc6f1b79aadb8acfb560065e0d193aed14231ad
BenMoon <brombh@desy.de>
2022-03-16 18:19:43 +0100

    Add updsampler to thread

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
-use std::alloc::handle_alloc_error;
 use std::net::UdpSocket;
-use std::ops::Add;
-use std::sync::mpsc::{channel, Receiver};
-use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::mpsc::{channel, Receiver, Sender};
+use std::sync::Arc;
 use std::thread::{spawn, JoinHandle};
 use std::{thread, time};
 
 use rayon::prelude::*;
 
-fn udp_sampler() {
+fn udp_sampler(sender: Sender<Vec<u64>>, cancle_flag: Arc<AtomicBool>) {
     let socket = UdpSocket::bind("127.0.0.1:50000").expect("couldn't bind to address");
 
     let mut bytes_recved: usize = 0;
-    let mut buffer_list = [[0_u8; 8]; 4];
+    let mut buffer_list = [[0_u8; 4096]; 4];
     let mut buffer_list_idx = 0;
     let mut buf = &mut buffer_list[buffer_list_idx];
     loop {
@@ -21,98 +19,97 @@ fn udp_sampler() {
             .recv(&mut buf[bytes_recved..])
             .expect("Didn't receive data");
 
-        if bytes_recved > 5 {
+        if bytes_recved > 128 {
             buffer_list_idx += 1;
             buffer_list_idx %= 4;
 
-            println!("index: {}", buffer_list_idx);
-            println!("{:?}", &buf[..bytes_recved]);
-            println!(
-                "{:?} bytes received, data: {:?}",
-                bytes_recved, &mut buffer_list
-            );
+            sender
+                .send(
+                    (0..bytes_recved / 8)
+                        .into_par_iter()
+                        .map(|i| {
+                            u64::from_le_bytes(
+                                buf[i * 8..i * 8 + 8]
+                                    .try_into()
+                                    .expect("slice with incorrect length"),
+                            )
+                        })
+                        .collect::<Vec<u64>>(),
+                )
+                .ok();
 
             buf = &mut buffer_list[buffer_list_idx];
             bytes_recved = 0;
         }
+        if cancle_flag.load(Ordering::SeqCst) {
+            println!("End UPD Sampler");
+            break;
+        }
     }
 }
 
-fn packet_processor() {
-    let data_recved: Vec<u8> = (0..8 * 256)
-        .enumerate()
-        .map(|(i, _)| if i % 8 == 0 { (i / 8) as u8 } else { 0 })
-        .collect();
-    let mut data_converted = [0_u64; 8];
-
-    for i in 0..8 {
-        //data_recved.len()/8 {
-        data_converted[i] = u64::from_le_bytes(
-            data_recved[i * 8..i * 8 + 8]
-                .try_into()
-                .expect("slice with incorrect length"),
-        );
-    }
-    println!("{:?}", data_converted);
-
-    let converted_iter = (0..8)
-        .into_par_iter()
-        .map(|i| {
-            u64::from_le_bytes(
-                data_recved[i * 8..i * 8 + 8]
-                    .try_into()
-                    .expect("slice with incorrect length"),
-            )
+fn packet_processor(pixdata: Vec<u64>) {
+    // extract trigger events from data stream
+    let triggers = pixdata
+        .iter()
+        .filter(|x| {
+            ((((**x & 0xF000000000000000) >> 60) & 0xF) == (0x4 | 0x6))
+          & ((((**x & 0x0F00000000000000) >> 56) & 0xF) == 0xF)
         })
+        .cloned()
         .collect::<Vec<u64>>();
-    println!("{:?}", converted_iter);
+        
+    // extract pixel events from data stream 
+    let pixels = pixdata
+        .into_iter()
+        .filter(|x| (((x & 0xF000000000000000) >> 60) & 0xF) == (0xA | 0xB))
+        .collect::<Vec<u64>>();
+
+    
 }
 
-fn start_udp_sampler(cancle_flag: Arc<AtomicBool>) -> (Receiver<Vec<u64>>, JoinHandle<std::io::Result<()>>) {
+fn start_udp_sampler(
+    cancle_flag: Arc<AtomicBool>,
+) -> (Receiver<Vec<u64>>, JoinHandle<std::io::Result<()>>) {
     let (sender, receiver) = channel();
 
     let handle = spawn(move || {
-        let mut i = 0;
-        loop {
-            println!("send {}", i);
-            sender.send(vec![i; 10]);
-            i += 1;
+        udp_sampler(sender, cancle_flag);
 
-            if cancle_flag.load(Ordering::SeqCst) { break; }
-            thread::sleep(time::Duration::from_millis(500));
-        }
         Ok(())
     });
 
     (receiver, handle)
 }
 
-fn start_packet_processor(cancle_flag: Arc<AtomicBool>, tpx_raw: Receiver<Vec<u64>>) -> JoinHandle<()> {
+fn start_packet_processor(
+    cancle_flag: Arc<AtomicBool>,
+    tpx_raw: Receiver<Vec<u64>>,
+) -> JoinHandle<()> {
     spawn(move || {
-    for (i, data) in tpx_raw.into_iter().enumerate() {
+        for (i, data) in tpx_raw.into_iter().enumerate() {
+            packet_processor(data);
+            /*
             println!("{} received {:?}", i, data);
 
             // pretend this work takes a bit longer
             thread::sleep(time::Duration::from_secs(1));
-            
+            */
             // have to duy an unmature death
-            if cancle_flag.load(Ordering::SeqCst) { 
+            if cancle_flag.load(Ordering::SeqCst) {
                 //println!("{:?} elements remain in queue", &tpx_raw.into_iter().count());
+                println!("End UPD Sampler");
                 break;
             }
         }
     })
-
 }
 
 //fn main() -> std::io::Result<()> {z
 fn main() {
- 
-
     let cancel_flag = Arc::new(AtomicBool::new(false));
     let cancle_udp = cancel_flag.clone();
-    let cancle_process= cancel_flag.clone();
-
+    let cancle_process = cancel_flag.clone();
 
     let (tpx_bin, h1) = start_udp_sampler(cancle_udp);
     let h2 = start_packet_processor(cancle_process, tpx_bin);
@@ -127,4 +124,32 @@ fn main() {
     // (As it happens, h2 and h3 can't fail: those threads
     // are pure in-memory data processing.)
     //r1?;
+}
+
+#[test]
+fn test_udpsampler() {
+    /// test which sends a few numbers and checks to receive them back
+    use bytemuck;
+
+    // first start udp_thread
+    let cancel_flag = Arc::new(AtomicBool::new(false));
+    let cancle_udp = cancel_flag.clone();
+    let cancle_process = cancel_flag.clone();
+
+    let (tpx_bin, h1) = start_udp_sampler(cancle_udp);
+
+    // send data
+    let socket = UdpSocket::bind("127.0.0.1:0").expect("binding socket failed");
+    socket
+        .connect("127.0.0.1:50000")
+        .expect("connect function failed");
+
+    let test_data = (0..64_u64).into_iter().collect::<Vec<u64>>();
+    let test_bytes_data: &[u8] = bytemuck::cast_slice(&test_data);
+    socket
+        .send(test_bytes_data)
+        .expect("sending of test data failed");
+
+    assert_eq!(test_data, tpx_bin.iter().next().unwrap());
+    assert_eq!(1, 2);
 }
--- udp_test_sender.py
+++ udp_test_sender.py
@@ -7,15 +7,15 @@ from tqdm import tqdm
 HOST, PORT = "127.0.0.1", 50000
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-data = np.arange(0, 10e6, dtype=np.uint8)
+data = np.arange(0, 10e6, dtype=np.uint64)
 print(data)
-size = 3
+size = 8
 
 for i in tqdm(range(50_000)):
     a = data[(i * size):((i + 1) * size)]
     print(a)
-    sendCmd = struct.pack("%sB" % size, *a)
-    #sendCmd = struct.pack("%sQ" % size, *a)
+    #sendCmd = struct.pack("%sB" % size, *a)
+    sendCmd = struct.pack("%sQ" % size, *a)
     sock.sendto(sendCmd, (HOST, PORT))
     # progress = int(i / total * 100)
     # if progress != 0 and progress % 5 == 0 and progress != last_progress:
